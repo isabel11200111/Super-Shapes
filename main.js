e38efa33e8ba5352cd37ff16d18c30918e3729f1
Isabell Oudman <isabelloudman@Isabells-MacBook-Air.local>
2020-05-15 12:34:06 -0400

    completed all animations except bottom right square sequence

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// duplicate function
+const duplicateHtml = (element, quantity) => {
+  const newContent = new Array(quantity).fill(element.innerHTML).join('')
+  element.innerHTML = newContent
+}
+// running duplicate function for 10 crosses
+const crosses = document.querySelector('#crosses')
+duplicateHtml(crosses, 10)
+
+anime({
+  targets: '#crosses path',
+  rotate: '1turn',
+  direction: 'alternate',
+  loop: true,
+  duration: 1000,
+  easing: 'easeInOutSine',
+  delay: (el, i, l) => i* 100,
+});
+
 anime({
   targets: '#tunnel circle',
   scale: 1.1,
@@ -7,7 +26,7 @@ anime({
   easing: 'easeInOutSine',
   // delay applied as a static value as well as a function. when used as a function it loops through every individual single item
   // individual element, index, Length
-delay: (el, i, l) => i* 50,
+  delay: (el, i, l) => i* 50,
   //new vs old js ^
   // delay: function(el, i, l) {
   //   return i * 50;
@@ -17,8 +36,75 @@ delay: (el, i, l) => i* 50,
 anime({
   targets:'.conveyor',
   translateX: '-50%',
-  duration: 1500,
+  duration: 2000,
   loop: true,
   easing: 'linear',
   autoplay: true,
+})
+
+
+const zigZagPath = document.querySelector('#zigzag path');
+const zigZagOffset = anime.setDashoffset(zigZagPath);
+  zigZagPath.setAttribute('stroke-dashoffset', zigZagOffset);
+  anime({
+    targets: zigZagPath,
+    strokeDashoffset: [zigZagOffset, 0],
+    duration: 3000,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
+
+
+const wavePath = document.querySelector('#wave path')
+const waveOffset = anime.setDashoffset(wavePath)
+wavePath.setAttribute('stroke-dashoffset', waveOffset);
+anime({
+  targets: wavePath,
+  strokeDashoffset: [0, waveOffset],
+  duration: 2000,
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInOutSine',
+});
+
+
+// running duplicate function for 40 square  dots
+duplicateHtml(document.querySelector('#dots'),40)
+const dots = document.querySelectorAll('#dots .dot')
+
+dots.forEach(dot => {
+  anime({
+    targets:dot,
+    rotate: (el, i) => anime.random(90, 360),
+    duration: (el, i) => anime.random(250, 750),
+    loop: true,
+    easing: 'easeInOutSine',
+    direction: 'alternate'
+  })
+});
+
+// running duplicate funtion for 20 flashing circle dots
+duplicateHtml(document.querySelector('#circles'),20)
+anime({
+  targets:'#circles .dot',
+  scale: [0, 1.2],
+  delay: (el, i) => i * 100,
+  duration:250,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate'
+})
+
+
+// four rectangles of flashing colours
+anime({
+  targets:'#flashes .flash',
+  backgroundColor:(el, i) => ['#fff636', '#cb89fc', '#fc3035', '#77ebfd'][i],
+  delay:(el, i) => anime.random(50, 100),
+  duration: 500,
+  loop:true,
+  direction: 'alternate',
+  easing:'easeInOutSine'
 })
